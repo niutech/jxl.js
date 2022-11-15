926eb27ae1883a1882f90466191ca65e9d384d5a
niutech <jerzyglowacki@gmail.com>
2022-11-15 14:23:21 +0100

    Use <canvas> instead of <img>

--- jxl.js
+++ jxl.js
@@ -1,23 +1,24 @@
 (function () {
-  function imgDataToURL(imageData) {
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    canvas.width = imageData.width;
-    canvas.height = imageData.height;
-    ctx.putImageData(imageData, 0, 0);
-    return canvas.toDataURL();
+  async function imgDataToCanvas(img, imgData) {
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width === 1 ? imgData.width : img.width;
+    canvas.height = img.height === 1 ? imgData.height : img.height;
+    canvas.className = img.className;
+    canvas.id = img.id;
+    canvas.dataset.jxlSrc = img.dataset.jxlSrc;
+    const imgBitmap = await window.createImageBitmap(imgData, {resizeWidth: canvas.width, resizeHeight: canvas.height});
+    canvas.getContext('2d').drawImage(imgBitmap, 0, 0);
+    img.replaceWith(canvas);
   }
 
   async function decode(img) {
-    const url = img.src;
+    img.dataset.jxlSrc = img.src;
     img.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='; // blank image
-    const res = await fetch(url);
+    const res = await fetch(img.dataset.jxlSrc);
     const image = await res.arrayBuffer();
     const worker = new Worker('jxl_dec.js');
-    worker.postMessage({url, image});
-    worker.addEventListener('message', m => {
-      img.src = imgDataToURL(m.data.result);
-    });
+    worker.postMessage({url: img.dataset.jxlSrc, image});
+    worker.addEventListener('message', m => imgDataToCanvas(img, m.data.result));
   }
 
   new MutationObserver(mutations => {
