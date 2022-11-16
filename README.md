e92e951e56007234c25b7b2af527b22cb17baf3a
niutech <jerzyglowacki@gmail.com>
2022-11-16 01:53:18 +0100

    Add support for CSS background-image and Cache API

--- README.md
+++ README.md
@@ -4,20 +4,18 @@ This is a JPEG XL decoder in JavaScript using WebAssembly implementation from th
 
 ## Usage
 
-Just insert your JPEG XL images to your HTML document as usual: `<img src="image.jxl" alt="..." width="..." height="...">` and append JXL.js script to the `<head>` of your web page:
+Just insert your JPEG XL images to your HTML document as usual: `<img src="image.jxl" alt="..." width="..." height="...">` and append the minified JXL.js script to the `<head>` of your web page:
 
-`<script src="jxl.js"></script>`
+`<script src="jxl.min.js"></script>`
 
-JXL.js uses Mutation Observer to watch for `<img>` tags being added to the DOM and it decodes them as they appear using WebAssembly decoder in Web Worker.
+JXL.js uses Mutation Observer to watch for `<img>` tags being added to the DOM as well as CSS background images and it decodes them as they appear using WebAssembly decoder in Web Worker. JXL.js leverages Cache API for faster subsequent page views.
 
 #### [See the demo](https://niutech.github.io/jxl.js/)
 
 ## To do
 
 - `<picture>` HTML element
-- `background-image` CSS property
 - performance improvements
-- image caching
 
 ## License
 
--- jxl.js
+++ jxl.js
@@ -1,4 +1,12 @@
 (function () {
+  function imgDataToURL(img, imgData) {
+    var canvas = document.createElement('canvas');
+    canvas.width = imgData.width;
+    canvas.height = imgData.height;
+    canvas.getContext('2d').putImageData(imgData, 0, 0);
+    img.style.backgroundImage = 'url("' + canvas.toDataURL() + '")';
+  }
+
   async function imgDataToCanvas(img, imgData) {
     const canvas = document.createElement('canvas');
     canvas.width = img.width === 1 ? imgData.width : img.width;
@@ -11,20 +19,36 @@
     img.replaceWith(canvas);
   }
 
-  async function decode(img) {
-    img.dataset.jxlSrc = img.src;
-    img.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='; // blank image
-    const res = await fetch(img.dataset.jxlSrc);
+  async function decode(img, isCSS) {
+    const jxlSrc = img.dataset.jxlSrc = isCSS ? getComputedStyle(img).backgroundImage.slice(5, -2) : img.currentSrc;
+    img.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='; // blank 1x1 image
+    const cache = await caches.open('jxl');
+    const cachedImg = await cache.match(jxlSrc);
+    if (cachedImg) {
+      const cachedImgData = new ImageData(new Uint8ClampedArray(await cachedImg.arrayBuffer()), cachedImg.headers.get('width'), cachedImg.headers.get('height'));
+      isCSS ? imgDataToURL(img, cachedImgData) : imgDataToCanvas(img, cachedImgData);
+      return;
+    }
+    const res = await fetch(jxlSrc);
     const image = await res.arrayBuffer();
     const worker = new Worker('jxl_dec.js');
-    worker.postMessage({url: img.dataset.jxlSrc, image});
-    worker.addEventListener('message', m => imgDataToCanvas(img, m.data.result));
+    worker.postMessage({jxlSrc, image});
+    worker.addEventListener('message', m => {
+      const imgData = m.data.result;
+      isCSS ? imgDataToURL(img, imgData) : imgDataToCanvas(img, imgData);
+      cache.put(jxlSrc, new Response(imgData.data, {headers: {'content-type': 'image/jxl', width: imgData.width, height: imgData.height}}));
+    });
   }
 
   new MutationObserver(mutations => {
     mutations.forEach(mutation => {
-      Array.prototype.filter.call(mutation.addedNodes, node => node instanceof HTMLImageElement && node.src.endsWith('.jxl')).forEach(img => {
-        img.onerror = () => decode(img);
+      Array.prototype.filter.call(mutation.addedNodes,
+        el => (el instanceof HTMLImageElement && el.src.endsWith('.jxl')) || (el instanceof Element && getComputedStyle(el).backgroundImage.endsWith('.jxl")')))
+      .forEach(el => {
+        if (el instanceof HTMLImageElement)
+          el.onerror = () => decode(el, false);
+        else
+          decode(el, true);
       });
     });
   }).observe(document.documentElement, {subtree: true, childList: true});
